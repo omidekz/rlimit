7360f1d28139eebc7e563fd11d1fc48011a22fcd
omidekz <omidekz@gmail.com>
2022-01-10 00:06:17 +0330

    new ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="farlimit",
-    version="0.0.1",
+    version="0.1.1",
     author="Omid Karimzade",
     author_email="omidekz@gmail.com",
     description="FastAPIRateLimit",
