72efdae337c8f5bcb3e9149e36d867d5b08f1a28
omidekz <omidekz@gmail.com>
2022-01-16 02:19:36 +0330

    remove request first arg constraint and add custom identifier feat

--- farlimit/limiter.py
+++ farlimit/limiter.py
@@ -1,3 +1,4 @@
+from ctypes import Union
 import functools
 from typing import Callable, ClassVar, Optional, Type
 from starlette.requests import Request
@@ -25,10 +26,14 @@ class Limiter(Per):
     per: int
     memory: ClassVar[Type[Memory]] = Memory
     ex: Type[HTTPException] = HTTPException
-    identifier: Callable[[Request], str] = identifier
-    key_maker: Optional[Callable[[Request, dict], str]]
+    identifier: Callable[[Union[Request, dict]], str] = identifier
+    key_maker: Callable[[dict], str] = lambda ka: ''
     request_parameter_name: str = 'request'
     message: str = "too many requests"
+
+    @property
+    def has_default_identifier(self):
+        return self.identifier == identifier
 
     @classmethod
     def init(cls, memory=Memory):
@@ -37,8 +42,11 @@ class Limiter(Per):
     def __call__(self, func):
         @functools.wraps(func)
         def wrapper(**kwargs):
-            req = kwargs.get(self.request_parameter_name)
-            key = self.identifier(req) + ((':' + self.key_maker(req, kwargs)) if self.key_maker else '')
+            embed_key = self.identifier(kwargs[self.request_parameter_name]) \
+                        if self.has_default_identifier \
+                        else self.identifier(kwargs)
+            custom_key = self.key_maker(kwargs)
+            key = f'{embed_key}:{custom_key}'
             times = self.memory.inc(key, ttl=self.per)
             if times > self.times:
                 raise HTTPException(429, self.message)
