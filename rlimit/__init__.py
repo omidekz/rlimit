b4d45fa38951ff78e185b63ecbb90c41826b1a43
omidekz <omidekz@gmail.com>
2023-02-21 13:24:29 +0330

    change import

--- rlimit/__init__.py
+++ rlimit/__init__.py
@@ -1,1 +1,6 @@
-from .base_limiter import BaseLimiter as Limiter, BaseMemory, ExpiringDictMemory
+from per import Per
+from memory_abc import BaseMemory
+from expiringdict_memory import ExpiringDictMemory
+from base_limiter import BaseLimiter
+from base_limiter import BaseLimiter as Limiter
+
